871145a87856fecda0cfc0bac089490bd4838e1f
山本憲 <em072029@gmail.com>
2017-06-03 09:24:35 +0900

    バグ修正

--- polls/views.py
+++ polls/views.py
@@ -4,7 +4,7 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 
 # Create your views here.
-from djnago.http import HttpResponse
+from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("hello,world. you're at the poll index")
