7df46c88fb88be9d057ac4d5e858dd081772b30a
山本憲 <em072029@gmail.com>
2017-06-04 15:02:04 +0900

    404エラー取得

--- polls/views.py
+++ polls/views.py
@@ -2,21 +2,29 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+from django.http import Http404
 
-# Create your views here.
-from django.http import HttpResponse
-from models import Question
+from .models import Question
+
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    output = ', '.join([q.question_text for q in latest_question_list])
-    print(output)
-    return HttpResponse(output)
+    context = {
+        'latest_question_list': latest_question_list
+    }
+    return render(request, 'polls/index.html', context)
 
 
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
-
+    try:
+        question = Question.objects.get(pk=question_id)
+        context = {
+            'question': question
+        }
+        print(request)
+    except Question.DoesNotExist:
+        raise Http404('Question does not exist')
+    return render(request, 'polls/detail.html', context)
 
 def results(request, question_id):
     response = "You're looking at the results of question %s."
