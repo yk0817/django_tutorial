5e42117d79ccddb314ec04b553009998407797e8
山本憲 <em072029@gmail.com>
2017-06-04 14:00:10 +0900

    バグ取り

--- polls/urls.py
+++ polls/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     # ex: /polls/5/
     url(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
     # ex: /polls/5/results/
-    url(r'^(?P<question_id>[0-9]+)/results/$',views.result,name='results')
+    url(r'^(?P<question_id>[0-9]+)/results/$',views.result,name='results'),
     # ex: /polls/5/vote/
     url(r'^(?P<question_id>[0-9]+)/vote/$',views.vote,name='vote'),
 ]
