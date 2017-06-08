eeef8c3337e91260badf81ee8a05777b1a1f6949
山本憲 <em072029@gmail.com>
2017-06-08 19:30:46 +0900

    修正

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # django_tutorial
 
+$ python manage.py runserver
 
 ##何をやる？
 [django](https://docs.djangoproject.com/en/1.11/contents/ "djangoチュートリアル")
--- polls/views.py
+++ polls/views.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render,get_object_or_404
-from django.http import HttpResponseRedirect
+from django.shortcuts import get_object_or_404,render
+from django.http import HttpResponseRedirect, HttpResponse
 from django.urls import reverse
 from django.views import generic
 
