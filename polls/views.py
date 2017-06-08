da6c7782e2e0ad9a29aa0c8e3e6ed6bb0bc3457f
山本憲 <em072029@gmail.com>
2017-06-08 20:18:40 +0900

    終了

--- polls/views.py
+++ polls/views.py
@@ -1,41 +1,42 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-
-from django.shortcuts import get_object_or_404,render
-from django.http import HttpResponseRedirect, HttpResponse
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponseRedirect
 from django.urls import reverse
 from django.views import generic
 
 from .models import Question, Choice
-import sys
+
 
 class IndexView(generic.ListView):
     template_name = 'polls/index.html'
     context_object_name = 'latest_question_list'
-    
+
     def get_queryset(self):
         return Question.objects.order_by('-pub_date')[:5]
+
 
 class DetailView(generic.DetailView):
     model = Question
     template_name = 'polls/detail.html'
 
+
 class ResultsView(generic.DetailView):
     model = Question
     template_name = 'polls/results.html'
 
-def vote(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
+
+def vote(request, pk):
+    question = get_object_or_404(Question, pk=pk)
     try:
         selected_choice = question.choice_set.get(pk=request.POST['choice'])
-    except (KeyError,Choice.DoesNotExist):
-        return render(request, 'polls/detail.html',{
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, 'polls/detail.html', {
             'question': question,
-            'error_message': "You did not select a choice"
+            'error_message': "You didn't select a choice"
         })
     else:
         selected_choice.votes += 1
         selected_choice.save()
-        
+
         return HttpResponseRedirect(
-            reverse('polls:results', args=(question.id,)))
+            reverse('polls:results', args=(question.id,))
+        )
